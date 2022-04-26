0c181c15bb80c35b3ff0c1debc3a32cd7a5dc70b
gos1004 <nnv711@gmail.com>
2022-04-26 14:21:58 +0900

    타입스크립트 시작

---
+++ getting-started/README.md
@@ -0,0 +1,6 @@
+# getting-started
+- 타입스크립트를 설치한다.
+`npm i typescript -g`
+
+- ts파일을 js파일로 생성한다.
+  `tsc index.ts`
--- why-ts/sample.ts
+++ why-ts/sample.ts
@@ -1,6 +1,6 @@
 
-function add(a: number, b: number) {
+function add1(a: number, b: number) {
     return a+b;
 }
-var result = add(10,20);
+var result = add1(10,20);
 result.toLocaleString();
